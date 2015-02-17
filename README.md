61669545d95a87f9f2db60d52957b019f169889a
lilxelo <cheloq@gmail.com>
2015-02-17 19:17:53 +0100

    Readme.md and unit test completed.

--- README.md
+++ README.md
@@ -5,6 +5,12 @@ Añade a [Date.prototype de JS](https://developer.mozilla.org/en-US/docs/Web/Jav
 Especificamos al método el formato en el que devolverá el objeto date.
 [npm](https://www.npmjs.com/package/date.format)
 
+```javascript
+Date.prototype.format( stringDate [, booleanUTC]);
+````
++ stringDate: string con el formato de sustitución de [String.format](#https://github.com/bifuer/String.format)
++ booleanUTC: boolean. true si el date es UTC. False para GMT.
+
 
 ```javascript
 require('date.format');
@@ -17,20 +23,20 @@ console.log(date.format("UTC: Day {YY}/{MM}/{DD} at {hh}:{mm}:{ss} and {mss}ms",
 
 /*
 
->> Local: Day 2015/1/15 at 20:44:58 and 803ms
->> Local: Day 2015/01/15 at 20:44:58 and 0803ms
->> UTC: Day 2015/1/15 at 19:44:58 and 803ms
->> UTC: Day 2015/01/15 at 19:44:58 and 0803ms
+>> Local: Day 2015/1/5 at 20:44:58 and 803ms
+>> Local: Day 15/01/05 at 20:44:58 and 0803ms
+>> UTC: Day 2015/1/5 at 19:44:58 and 803ms
+>> UTC: Day 15/01/15 at 19:44:58 and 0803ms
 
 */
 ```
-+ **Y**: Año
-+ **M**: Mes 
-+ **D**: Día
-+ **h**: hora
-+ **m**: minuto
-+ **s**: segundo
-+ **ms**: milisegundo
++ **Y** y **YYYY**: Año completo. **YY** Año 2 dígitos, 00-99
++ **M**: Mes sin ceros iniciales 1-12. **MM** Mes en 2 dígitos. 
++ **D**: Día sin ceros iniciales 1-31. **DD** Día en 2 dígitos.
++ **h**: hora sin ceros iniciales 0-23. **hh** hora en 2 dígitos.
++ **m**: minutos sin ceros iniciales 0-59. **mm** minutos en 2 dígitos.
++ **s**: segundos sin ceros iniciales 0-59. **ss** segundos en 2 dígitos.
++ **ms**: milisegundos sin ceros iniciales 0-999. **mss** milisegundos 4 dígitos. 
 
 
 ##Licence
--- test/test.js
+++ test/test.js
@@ -3,12 +3,107 @@ require("../");
 
 describe("Date.format ----- ",function(){ 
   describe("---",function(){ 
-    it("Unit Test for diferent values",function(){     
+    it("Unit Test for diferent completes values & GMT",function(){     
      
-     var date = new Date('1981-12-11T13:55:00');
+      var date = new Date();
+      date.setFullYear(1981);
+      date.setMonth(12-1);
+      date.setDate(11);
+      date.setHours(13);
+      date.setMinutes(54);
+      date.setSeconds(59);
+      date.setMilliseconds(1);
+    
       test
-        .value(date.format("{Y}/{MM}/{D}"))
-          .is('1981/12/11')
+        .value(date.format("{Y}/{MM}/{D} {h}:{m}:{s} {ms}"))
+          .is('1981/12/11 13:54:59 1')
+        .value(date.format("{YY}/{M}/{D} {hh}:{mm}:{ss} {mss}"))
+          .is('81/12/11 13:54:59 0001')
+        .value(date.format("{Y}/{MM}/{DD} {h}:{mm}:{ss} {ms}"))
+          .is('1981/12/11 13:54:59 1');
      })
+
+    it("Unit Test for all possible datas <10 & GMT",function(){     
+
+      var date = new Date();
+      date.setFullYear(2100);
+      date.setMonth(5-1);
+      date.setDate(1);
+      date.setHours(1);
+      date.setMinutes(1);
+      date.setSeconds(5);
+      date.setMilliseconds(436);
+
+      test
+        .value(date.format("{Y}/{MM}/{D} {h}:{m}:{s} {ms}"))
+          .is('2100/05/1 1:1:5 436')
+        .value(date.format("{YY}/{M}/{D} {h}:{m}:{s} {mss}"))
+          .is('00/5/1 1:1:5 0436')
+        .value(date.format("{Y}/{MM}/{DD} {hh}:{mm}:{ss} {ms}"))
+          .is('2100/05/01 01:01:05 436');
+
+     })
+
+    it("Unit Test for diferent completes values & GMT",function(){     
+     
+      var date = new Date();
+      date.setUTCFullYear(1981);
+      date.setUTCMonth(12-1);
+      date.setUTCDate(11);
+      date.setUTCHours(13);
+      date.setUTCMinutes(54);
+      date.setUTCSeconds(59);
+      date.setUTCMilliseconds(899);
+    
+      test
+        .value(date.format("{Y}/{MM}/{D} {h}:{m}:{s} {ms}", true))
+          .is('1981/12/11 13:54:59 899')
+        .value(date.format("{YY}/{M}/{D} {hh}:{mm}:{ss} {mss}", true))
+          .is('81/12/11 13:54:59 0899')
+        .value(date.format("{Y}/{MM}/{DD} {h}:{mm}:{ss} {ms}", true))
+          .is('1981/12/11 13:54:59 899');
+     })
+
+    it("Unit Test for all possible datas <10 & UTC",function(){     
+
+      var date = new Date();
+      date.setUTCFullYear(2100);
+      date.setUTCMonth(5-1);
+      date.setUTCDate(1);
+      date.setUTCHours(1);
+      date.setUTCMinutes(1);
+      date.setUTCSeconds(5);
+      date.setUTCMilliseconds(1);
+
+      test
+        .value(date.format("{Y}/{MM}/{D} {h}:{m}:{s} {ms}", true))
+          .is('2100/05/1 1:1:5 1')
+        .value(date.format("{YY}/{M}/{D} {h}:{m}:{s} {mss}", true))
+          .is('00/5/1 1:1:5 0001')
+        .value(date.format("{Y}/{MM}/{DD} {hh}:{mm}:{ss} {ms}", true))
+          .is('2100/05/01 01:01:05 1');
+
+     })
+
+    it("Zeros & UTC",function(){     
+
+      var date = new Date();
+      date.setUTCFullYear(2000);
+      date.setUTCMonth(1-1);
+      date.setUTCDate(1);
+      date.setUTCHours(0);
+      date.setUTCMinutes(0);
+      date.setUTCSeconds(0);
+      date.setUTCMilliseconds(0);
+
+      test
+        .value(date.format("{h}:{m}:{s} {ms}", true))
+          .is('0:0:0 0')
+        .value(date.format("{hh}:{mm}:{ss} {mss}", true))
+          .is('00:00:00 0000')
+
+     })
+
+
   })
 });
